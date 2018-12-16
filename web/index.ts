837a05dcb59a50494499c1855c704b8961c125b1
mtgto <hogerappa@gmail.com>
2018-12-16 17:32:46 +0900

    Add the feature to drag

--- web/index.ts
+++ web/index.ts
@@ -6,9 +6,39 @@ let originLongitude: number = 139.76699;
 
 const canvas: HTMLCanvasElement = document.getElementById("canvas") as HTMLCanvasElement;
 
+let isDragging: boolean = false;
+let lastX: number = 0;
+let lastY: number = 0;
+
+canvas.onmousedown = (e) => {
+    isDragging = true;
+    const offsetX = canvas.getBoundingClientRect().left;
+    const offsetY = canvas.getBoundingClientRect().top;
+
+    lastX = e.clientX - offsetX;
+    lastY = e.clientY - offsetY;
+}
+
+canvas.onmousemove = (e) => {
+    if (isDragging){
+        const offsetX = canvas.getBoundingClientRect().left;
+        const offsetY = canvas.getBoundingClientRect().top;
+        originLongitude += (lastX - (e.clientX - offsetX)) * 0.001;
+        originLatitude += (e.clientY - offsetY - lastY) * 0.001;
+        lastX = e.clientX - offsetX;
+        lastY = e.clientY - offsetY;
+        draw(0);
+    }
+}
+
+canvas.onmouseup = () => {
+    isDragging = false;
+}
+
 const draw = (time: number) => {
     if (canvas.getContext) {
         const context: CanvasRenderingContext2D = canvas.getContext("2d") as CanvasRenderingContext2D;
+        context.clearRect(0, 0, canvas.width, canvas.height);
         for (const oaza of tokyo) {
             context.beginPath();
             for (let i = 0; i + 1 < oaza.sphere.length; i += 2) {
